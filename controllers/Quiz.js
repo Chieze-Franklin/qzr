a8bb3ef231486031071c0bc37d467ad73b693e58
chieze-franklin <chieze.franklin@gmail.com>
2022-01-25 04:29:44 +0100

    create quiz

--- controllers/Quiz.js
+++ controllers/Quiz.js
@@ -1,19 +1,56 @@
 const QuizModel = require("../models/Quiz");
 
-const create = async (req, res) => {
-    const quiz = new QuizModel({
-        title: "A test quiz",
-    });
-    await quiz.save();
-    res.json({ quiz });
+const create = async (req, res, next) => {
+    try {
+        // TODO: validate req.body
+        const quiz = new QuizModel({
+            user_id: req.user._id,
+            title: req.body.title,
+            questions: req.body.questions,
+        });
+        await quiz.save();
+        res.json({ quiz });
+    } catch(err) {
+        next(err);
+    }
 };
 
-const findMany = async (req, res) => {
-    const quizzes = await QuizModel.find();
-    res.json(quizzes);
+const find = async (req, res, next) => {
+    try {
+        // TODO: validate req.query
+
+        let quizzes = [];
+
+        if (req.query.mine) {
+            quizzes = await QuizModel.find({ user_id: req.user._id }).select("-questions");
+        } else {
+            quizzes = await QuizModel.find({ user_id: { $ne: req.user._id }, published: true }).select("-questions");
+        }
+
+        res.json(quizzes);
+    } catch(err) {
+        next(err);
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        // TODO: validate req.params and req.body
+
+        // TODO: add, update, delete questions
+
+        const reqBodyWithoutQuestions = { ...req.body };
+        delete reqBodyWithoutQuestions.questions;
+        const result = await QuizModel.updateOne({ _id: req.params.id, user_id: req.user._id }, reqBodyWithoutQuestions);
+
+        res.json({ result });
+    } catch(err) {
+        next(err);
+    }
 };
 
 module.exports = {
     create,
-    findMany,
+    find,
+    update,
 };
--- routes/Quiz.js
+++ routes/Quiz.js
@@ -3,7 +3,8 @@ const quizController = require("../controllers/Quiz");
 
 const router  = express.Router();
 
-router.get("/q", quizController.findMany);
+router.get("/q", quizController.find);
 router.post("/q", quizController.create);
+router.patch("/q/:id", quizController.update);
 
 module.exports = router;
